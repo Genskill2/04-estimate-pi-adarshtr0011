13483986a2498e2f451d79cbcd708f8e282b4772
adarshtr0011 <84710801+adarshtr0011@users.noreply.github.com>
2021-06-04 11:29:04 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -9,7 +9,8 @@ int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
+    if (!(fabs(pi - M_PI) > 0.15)) 
+    {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
